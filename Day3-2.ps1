38bd14d479dbde0f1de4f0c3e995e8d78cb62e4d
rstetzer <614592+rstetzer@users.noreply.github.com>
2021-12-30 17:11:39 -0800

    Day 3 Done

--- Day3-2.ps1
+++ Day3-2.ps1
@@ -13,7 +13,7 @@
 #Repeat
 
 #We are using ArrayList since it lets us delete entries in the Array directly
-[System.Collections.ArrayList]$Values = Import-Csv .\Day3-Demo.csv -header Entry
+[System.Collections.ArrayList]$Values = Import-Csv .\Day3.csv -header Entry
 
 
 #This was the loop statement I was looking for in Day3-1
@@ -21,10 +21,9 @@
 #This is setting the number of digits in length of the CSV entries
 $ValueCount = $Values.Entry[0].Length
 #Decrementing the number by one since the indexes start at 0 instead of 1
-$ValueCount--
 
 #This will loop the whole process the number of times to match the length of the CSV entries
-for ($indexLength = 0; $indexLength -lt $Value.Count; $indexLength++)
+for ($indexLength = 0; $indexLength -lt $ValueCount; $indexLength++)
 {
 
 #Reset the variables for each pass
@@ -35,7 +34,7 @@ $One = 0
 $Keeper =
 
 #Loop for each entry in the CSV, looping for the number of entries in the CSB
-for ($index = 0; $index -lt $ValuesCount; $index++)
+for ($index = 0; $index -le $ArrayLength; $index++)
 {
 #Convert the entry to string to run SubString actions on it
     $l = $Values[$index].Entry.ToString()
@@ -63,12 +62,64 @@ If ($l.Substring($indexLength,1) -ne $Keeper)
 
 #If we have gotten down to only one entry
 If ($Values.count -eq 1)
-{Break
-Write-Host $Values}
-Write-Host Value $indexLength
-Write-Host Zero $Zero
-Write-Host One $One
-Write-Host Keeper $Keeper
-Write-Host $Values
+{
+Write-Host O2 $Values
+Break}
+}
+
+
+#We are using ArrayList since it lets us delete entries in the Array directly
+[System.Collections.ArrayList]$Values = Import-Csv .\Day3.csv -header Entry
+
+
+#This was the loop statement I was looking for in Day3-1
+
+#This is setting the number of digits in length of the CSV entries
+$ValueCount = $Values.Entry[0].Length
+#Decrementing the number by one since the indexes start at 0 instead of 1
+
+#This will loop the whole process the number of times to match the length of the CSV entries
+for ($indexLength = 0; $indexLength -lt $ValueCount; $indexLength++)
+{
+
+#Reset the variables for each pass
+$ArrayLength = $Values.count
+$ArrayLength--
+$Zero = 0
+$One = 0
+$Keeper =
+
+#Loop for each entry in the CSV, looping for the number of entries in the CSB
+for ($index = 0; $index -le $ArrayLength; $index++)
+{
+#Convert the entry to string to run SubString actions on it
+    $l = $Values[$index].Entry.ToString()
+#Looking at the digit matching the loop we are in and incrementing the count on Zero or One
+If ($l.Substring($indexLength,1) -eq 1)
+    {$One ++}
+Else {
+    $Zero ++}
+}
+#Deciding which number has a higher count, giving Zero precident on ties for CO2
+If ($One -lt $Zero)
+    {$Keeper = 1}
+else {
+    $Keeper = 0}
+
+# I need to reverse this lookup. When deleting an entry, the next number would slide back and get skipped when the index increments
+for ($index = $ArrayLength; $index -ge 0; $index--)
+{
+#Convert the entry to string to run SubString actions on it
+$l = $Values[$index].Entry.ToString()
+#Checking if the entry not equals the keeper value, remove the entry
+If ($l.Substring($indexLength,1) -ne $Keeper)
+{$Values.RemoveAt($index)}
+}
+
+#If we have gotten down to only one entry
+If ($Values.count -eq 1)
+{
+Write-Host CO2 $Values
+Break}
 }
 
