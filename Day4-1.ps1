f1cfe3f9eb874f6bf70accc6bd9dda6ea863b5b9
rstetzer <614592+rstetzer@users.noreply.github.com>
2021-12-30 18:55:36 -0800

    Brain dump for 4-1

---
+++ Day4-1.ps1
@@ -0,0 +1,10 @@
+#Import the list of boards into some kind of searchable array
+
+#Import the list of picks in to an array
+
+#Make a loop through each of the Picks in the array
+#Take the pick and look through each of the boards to see if there is a match
+#If there is a match, re-write the value to be zero
+#Sum each row and column in each board to see if the sum of the values equal zero
+    #Write the winning number to a variable, the winning board array number and Break
+#Add up all of the values on the winning board and multiply it by the winning number
