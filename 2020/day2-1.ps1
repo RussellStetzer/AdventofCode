e7dfbe1a96e59f7e2d615e959954db3e84059986
rstetzer <614592+rstetzer@users.noreply.github.com>
2022-10-28 22:34:53 -0700

    Update day2-1.ps1
    1 hour
    
    I got the code to work. I had to add quotes around $lettercase to get it to resolve the actual value vs the string. I am not sure why I don't need that everywhere, but maybe it is a double variable thing.
    
    I received the wrong answer since I did not account for the length of the rules to be more than one character long. Wrote some code to start parsing the string, putting values in to a variable until you reach the -, then increment the counter past the For loop to exit on next loop. Not working quite yet, but should.

--- 2020/day2-1.ps1
+++ 2020/day2-1.ps1
@@ -21,37 +21,57 @@ How many passwords are valid according to their policies?
 "@
 
 #We are probably going to need to set a header on the imported file to set Range, Letter, Sample or something
-$Array = import-csv .\day2-sample.csv -delimiter " " -Header Range, Letter, Password
+$Array = import-csv .\day2-data.csv -delimiter " " -Header Range, Letter, Password
 #We will need to set a count of how many correct entries we get
 [int]$CorrectPasswords = 0
 
 Foreach ($Entry in $Array)
 {
-[int]$lettercount = 0
+    [int]$lettercount = 0
+#I need to create logic to actually filter out the password range. My Range[0] and Range [2] don't account for multi-digit possibilities
+#Maybe process as a string, moving the characters to one variable looking for the -. When you find it, move to a differnt variable?    
+    [string]$stringlow = $null
+    [string]$stringhigh = $null
 
+    For ($count=0; $count -lt $Entry.Range.Length; $count++)
+    {
+        If ($Entry.Range[$count] -ne '-')
+        {
+        $stringlow = $stringlow + $Entry.Range[$count]
+        } else {
+        $count = $Entry.Range.Length
+        }
+    }
+
+    For ($count=($Entry.Range.length - 1); $count -ge 0; $count--)
+    {
+        If ($Entry.Range[$count] -ne '-')
+        {
+            $stringhigh = $Entry.Range[$count] + $stringlow
+        } else {
+            $count = -1
+        }
+    }
     #Write-Output $Entry.Letter[0]
     #Write-output $Entry.Password
     #Write-output $Entry.Password.Length
     #parse through the Password counting all of the letters matching the letter rule
     For ($count=0; $count -lt $Entry.Password.Length; $count++)
     {
-        If ($Entry.Password[$count] -match $Entry.Letter[0])
+        If ($Entry.Password[$count] -match $stringlow)
         {
             $lettercount++
         }
     }
-    write-output "letter count"$lettercount
-    Write-Output $Entry.Range[0]
-    Write-Output $Entry.Range[2]
 #See if $lettercount matches the critera given in the Range
-#The If statement is not working for some reason. switching it to -ge got it to work, but then it would not be correct.
-If ($Entry.Range[0] -le $lettercount)
-    {
-        Write-output "letter count is -ge"
-        If (-le $Entry.Range[2] $lettercount )
+#I am still not exactly sure why it worked, but I had to add double quotes to the $lettercount variable. Maybe when comparing two variables, it does something?
+#Supposedly, to have a variable expand to is info, rather than looking at the words written, you need a double quote.
+    If ("$lettercount" -ge $Entry.Range[0])
         {
-            $CorrectPasswords++
+            If ("$lettercount" -le $stringhigh)
+            {
+              $CorrectPasswords++
+          }
         }
-    }
 }
 Write-Output $CorrectPasswords
