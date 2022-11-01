7c237d9ae87721693c6bcea55d84351cec19d38e
rstetzer <614592+rstetzer@users.noreply.github.com>
2022-10-31 21:26:14 -0700

    Day3-2 partial
    1 hour
    
    Mostly written, troubleshooting why it is not getting a result bigger then 0. Plus VS Code was throwing errors, commit for safey.

--- 2020/day3-2.ps1
+++ 2020/day3-2.ps1
@@ -15,3 +15,46 @@ What do you get if you multiply together the number of trees encountered on each
 "@
 
 #For the last example, maybe keep a count of how many rown down you go. If even, increment the Column and row. If odd, only increment the row?
+
+$Passes = import-csv .\day3-input.csv -Header Right, Down
+$Array = import-csv .\day3-data.csv -Header Value
+#Starting Total at 1 so that we can multiply the later steps succesfully
+[int]$Total = 1
+#Using this count to accound for the Passes that have Down set to a value higher than 1
+[int]$Count = 0
+
+
+ForEach ($Pass in $Passes)
+{
+    #Reinitalize these values being unique to this Pass
+    $Column = 0
+    [int]$Hits = 0
+    ForEach ($Entry in $Array)
+    { 
+        #Increment the count to see if this pass is recorded for the ones with Down being higher than 1
+        $Count++
+        Write-Output $Count
+        #If matching, will record the hits and reset the count
+        If ($Count -match $Pass.Down)
+        {
+            If ($Entry.Value[$Column] -match "#")
+            {
+            $Hits++ 
+            }
+            #incrementing the column per loop
+            $Column = $Column + $Pass.Right
+            #looping back to the left once we get past the 31 digits of the row
+            If ($Column -ge 31)
+            {
+                $Column = $Column - 31
+            }
+            #Resets the counter to be incremented next pass
+            $Count = 0
+        } else {
+            #This should escape this pass of the entry allowing the count to increase next pass to be recorded
+            Break
+        }
+    }
+    $Total = $Total * $Hits
+}
+Write-Output $Total
